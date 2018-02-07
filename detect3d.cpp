521c4b68557673e298f25a3066c8eaa3e263936c
Zhangchao <839766550@qq.com>
2018-02-07 19:57:17 +0800

    temp

--- detect3d.cpp
+++ detect3d.cpp
@@ -64,6 +64,50 @@ Mat RegionGrow(Mat src, Point2i pt, int th)
 	return matDst.clone();
 }
 
+Mat RegionGrow1(Mat src, Point2i pt, int th)
+{
+	Point2i ptGrowing;                      //待生长点位置  
+	int nGrowLable = 0;                             //标记是否生长过  
+	int nSrcValue = 0;                              //生长起点灰度值  
+	int nCurValue = 0;                              //当前生长点灰度值  
+	Mat matDst = Mat::zeros(src.size(), CV_8UC1);   //创建一个空白区域，填充为黑色  
+	src.copyTo(matDst);
+	//生长方向顺序数据  
+	int DIR[8][2] = { { -1,-1 },{ 0,-1 },{ 1,-1 },{ 1,0 },{ 1,1 },{ 0,1 },{ -1,1 },{ -1,0 } };
+	vector<Point2i> vcGrowPt;                     //生长点栈  
+	vcGrowPt.push_back(pt);                         //将生长点压入栈中  
+	matDst.at<uchar>(pt.y, pt.x) = 255;               //标记生长点  
+	nSrcValue = src.at<uchar>(pt.y, pt.x);            //记录生长点的灰度值  
+
+	while (!vcGrowPt.empty())                       //生长栈不为空则生长  
+	{
+		pt = vcGrowPt.back();                       //取出一个生长点  
+		vcGrowPt.pop_back();
+
+		//分别对八个方向上的点进行生长  
+		for (int i = 0; i<9; ++i)
+		{
+			ptGrowing.x = pt.x + DIR[i][0];
+			ptGrowing.y = pt.y + DIR[i][1];
+			//检查是否是边缘点  
+			if (ptGrowing.x < 0 || ptGrowing.y < 0 || ptGrowing.x >(src.cols - 1) || (ptGrowing.y > src.rows - 1))
+				continue;
+
+			nGrowLable = matDst.at<uchar>(ptGrowing.y, ptGrowing.x);      //当前待生长点的灰度值  
+
+			if (nGrowLable == 0)                    //如果标记点还没有被生长  
+			{
+				nCurValue = src.at<uchar>(ptGrowing.y, ptGrowing.x);
+				if (abs(nSrcValue - nCurValue) < th)                 //在阈值范围内则生长  
+				{
+					matDst.at<uchar>(ptGrowing.y, ptGrowing.x) = 255;     //标记为白色  
+					vcGrowPt.push_back(ptGrowing);                  //将下一个生长点压入栈中  
+				}
+			}
+		}
+	}
+	return matDst.clone();
+}
 
 void detect3d::findModel(cv::Mat depthImage,string path,Point* matchLocation,float threshold)
 {
@@ -371,8 +415,8 @@ int detect3d::check3d(cv::Mat depthImage,cv::Mat silk2D)
 	imshow("imgunion", imgunion);
 
 	Point2i seed;
-	seed.x = 1;
-	seed.y = 1;
+	//seed.x = 1;
+	//seed.y = 1;
 	for (int i = 0; i<canny.rows; i++)
 	{
 		for (int j = 0; j<canny.cols; j++)
@@ -393,19 +437,28 @@ int detect3d::check3d(cv::Mat depthImage,cv::Mat silk2D)
 	imshow("imginter", imginter);
 	imshow("seedgrow", imgunion);
 	imgunion.copyTo(seedgrow);
-	//for (int i = 0; i<canny.rows; i++)
-	//{
-	//	for (int j = 0; j<canny.cols; j++)
-	//	{
-	//		blackMask.at<uchar>(i, j) = imgunion.at<uchar>(i, j) / 255 * blackMask.at<uchar>(i, j);
-	//	}
-	//}
-	//imshow("newblackMask", blackMask);
-	/*waitKey();*/
-	//Mask = Mask 
-	//waitKey();
 	ConnectEdge(Mask);
-	//imshow("Mask1", Mask);
+	Mat dep1;
+	depthImage.copyTo(dep1);
+	for (int i = 0; i<seedgrow.rows; i++)
+	{
+		for (int j = 0; j<seedgrow.cols; j++)
+		{
+			if (seedgrow.at<uchar>(i, j) == 0 )
+			{
+				seed.x = j;
+				seed.y = i;
+				dep1 = RegionGrow1(dep1, seed, 0);
+			}
+		}
+	}
+	imshow("hua", dep1);
+	waitKey();
+
+	Mat element1 = getStructuringElement(MORPH_RECT, Size(5, 5), Point(-1, -1));
+	erode(seedgrow, seedgrow, element1);
+	dilate(seedgrow, seedgrow, element1);
+	imshow("seedgrowerode", seedgrow);
 	vector<vector<Point> > contours;
 	vector<Vec4i> hierarchy;
 	findContours(seedgrow, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(-1, -1));
